572a64396f0d83c3c529cb181ba756e2686a8b90
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2018-12-23 21:05:17 -0800

    added observable

--- build.js
+++ build.js
@@ -20,29 +20,32 @@ const data = {
   work: chalk.white('Engineer @ SiFive'),
   // opensource: chalk.white('Node.js Community Committee ') + chalk.green('⬢'),
   twitter: chalk.gray('https://twitter.com/') + chalk.cyan('wavedrom'),
+  observable: chalk.gray('https://observablehq.com/@') + chalk.blueBright('drom'),
   npm: chalk.gray('https://npmjs.com/') + chalk.red('~drom'),
   github: chalk.gray('https://github.com/') + chalk.green('drom'),
   gitlab: chalk.gray('https://gitlab.com/') + chalk.redBright('drom'),
   linkedin: chalk.gray('https://linkedin.com/in/') + chalk.yellow('drom'),
   web: chalk.cyan('https://drom.io'),
   npx: chalk.red('npx') + ' ' + chalk.white('drom'),
-  labelWork: chalk.white.bold('    Work:'),
+  labelWork: chalk.white.bold('      Work:'),
   labelOpenSource: chalk.white.bold('Open Source:'),
-  labelTwitter: chalk.white.bold(' Twitter:'),
-  labelnpm: chalk.white.bold('     npm:'),
-  labelGitHub: chalk.white.bold('  GitHub:'),
-  labelGitLab: chalk.white.bold('  GitLab:'),
-  labelLinkedIn: chalk.white.bold('LinkedIn:'),
-  labelWeb: chalk.white.bold('     Web:'),
-  labelCard: chalk.white.bold('    Card:')
+  labelTwitter: chalk.white.bold('   Twitter:'),
+  labelObservable: chalk.white.bold('Observable:'),
+  labelnpm: chalk.white.bold('       npm:'),
+  labelGitHub: chalk.white.bold('    GitHub:'),
+  labelGitLab: chalk.white.bold('    GitLab:'),
+  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
+  labelWeb: chalk.white.bold('       Web:'),
+  labelCard: chalk.white.bold('      Card:')
 }
 
 // Actual strings we're going to output
 const newline = '\n'
 const heading = `${data.name} / ${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
-const opensourcing = `${data.labelOpenSource}  ${data.opensource}`
+// const opensourcing = `${data.labelOpenSource}  ${data.opensource}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
+const observing = `${data.labelObservable}  ${data.observable}`
 const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const gitlabing = `${data.labelGitLab}  ${data.gitlab}`
@@ -56,6 +59,7 @@ const output = heading + // data.name + data.handle
                working + newline + // data.labelWork + data.work
                // opensourcing + newline + newline + // data.labelOpenSource + data.opensource
                twittering + newline + // data.labelTwitter + data.twitter
+               observing + newline +
                npming + newline + // data.labelnpm + data.npm
                githubing + newline + // data.labelGitHub + data.github
                gitlabing + newline + // data.labelGitLab + data.gitlab
